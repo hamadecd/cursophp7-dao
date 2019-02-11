1cbc5c74b5947736454d77c9c988b4d42c11ec70
Samir Hamade <samircd9@gmail.com>
2019-02-11 14:25:17 -0200

    alteracao da classe usuario - metodo busca login

--- index.php
+++ index.php
@@ -6,8 +6,22 @@ require_once("config.php");
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);*/
 
-$root = new usuario();
-$root->loadById(3);
-echo $root;
+//carrega um usuario
+/*$user = new usuario();
+$user->loadById(3);
+echo $user;*/
+
+//carrega uma lista de usuarios
+/*$lista = usuario::getList();
+echo json_encode($lista);*/
+
+//carrega uma lista de usuarios buscando pelo login
+/*$search = usuario::search("lano");
+echo json_encode($search);*/
+
+//carrega um usuario usando o login e a senha
+$user = new usuario();
+$user->login("ciclano", "666666");
+echo $user;
 
 ?>
